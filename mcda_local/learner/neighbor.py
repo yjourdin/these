f875afab11a032141b808a6e273b10710938012e
Yann Jourdin <yann.jourdin@imt-atlantique.fr>
2023-12-11 10:11:16 +0100

    Add different probabilities to RandomNeighbor

--- mcda_local/learner/neighbor.py
+++ mcda_local/learner/neighbor.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from collections.abc import Sequence
 from typing import Generic, TypeVar
 
+from numpy import array
 from numpy.random import Generator
 
 T = TypeVar("T")
@@ -14,9 +15,16 @@ class Neighbor(Generic[T], ABC):
 
 
 class RandomNeighbor(Neighbor[T]):
-    def __init__(self, neighbors: Sequence[Neighbor[T]]):
+    def __init__(
+        self, neighbors: Sequence[Neighbor[T]], prob: Sequence[float] | None = None
+    ):
         self.neighbors = neighbors
+        if prob:
+            prob_array = array(prob)
+            self.prob = prob_array / prob_array.sum()
+        else:
+            self.prob = None
 
     def __call__(self, model: T, rng: Generator) -> T:
-        i = rng.integers(0, len(self.neighbors))
+        i = rng.choice(len(self.neighbors), p=self.prob)
         return self.neighbors[i](model, rng)
