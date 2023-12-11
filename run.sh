0763c1e1e0580d7d4f8bf0e47573c318f04ca5aa
Yann Jourdin <yann.jourdin@imt-atlantique.fr>
2023-12-11 11:48:14 +0100

    Add parallel task in run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 for m in 3 5 7; do
     for ke in 1 2 3; do
         for nbc in 10 20 30 40 50 60 70 80 90 100; do
-            python main.py @config.txt --M=$m --K-e=$ke --N-tr=$nbc --N-te=$nbc --N-bc=$nbc >>out.txt
+            python main.py @config.txt --M=$m --K-e=$ke --N-tr=$nbc --N-te=$nbc --N-bc=$nbc >>out.txt &
         done
     done
 done
