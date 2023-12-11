6d009978f388681444891764e316171e0f0398b2
Yann Jourdin <yann.jourdin@imt-atlantique.fr>
2023-12-11 17:13:03 +0100

    Files for evo experiment 2

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ for m in 7 11 15; do
     for ke in 1 2 3 4; do
         for nbc in 100 300 500 1000 2000; do
             for seed in 0 1 2 3 4 5 6 7 8 9; do
-                python main.py @config.txt --M=$m --K-e=$ke --N-tr=$nbc --N-te=$nbc --N-bc=$nbc --seed=$seed >>evo_out.csv &
+                python main.py @config.txt --M=$m --K-o=$ke --K-e=$ke --N-bc=$nbc --seed=$seed >>evo_out.csv &
             done
         done
     done
