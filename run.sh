cc31bb3c5f4cffe112d77b8077c6612bcf60f7c6
Yann Jourdin <yann.jourdin@imt-atlantique.fr>
2023-12-11 11:22:09 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 for m in 3 5 7; do
     for ke in 1 2 3; do
         for nbc in 10 20 30 40 50 60 70 80 90 100; do
-            python3 main.py @config.txt --m=$m --k-e=$ke --n-bc=$nbc >>out.txt
+            python main.py @config.txt --m=$m --k-e=$ke --n-bc=$nbc >>out.txt
         done
     done
 done
