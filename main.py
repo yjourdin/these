1dd91429859d16ba36dabf5fda5a8fd20e84ea87
Yann Jourdin <yann.jourdin@imt-atlantique.fr>
2023-12-11 10:11:53 +0100

    Add seeds dict and print all parameters in main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from typing import Any
+from dataclasses import fields
+from typing import Any, cast
 
+from numpy.random import SeedSequence, default_rng
 from scipy.stats import kendalltau
 
 from argument_parser import parse_args
 from generate_random import (
+    all_comparisons,
     random_alternatives,
     random_comparisons,
-    random_generator,
     random_rmp,
     random_srmp,
 )
@@ -28,57 +30,42 @@ from utils import midpoints
 ARGS = parse_args()
 
 
-# Create random generators
-default_rng, default_seed = random_generator(ARGS.seed)
-
-A_train_rng, A_train_seed = (
-    random_generator(ARGS.A_train_seed)
-    if ARGS.A_train_seed
-    else (default_rng, default_seed)
+# Create random seeds
+seeds: dict[str, int] = {"general": cast(int, SeedSequence(ARGS.seed).entropy)}
+seeds.update(
+    zip(
+        ["A_train", "model", "D_train", "learn", "A_test"],
+        SeedSequence(seeds["general"]).generate_state(5),
+    )
 )
-model_rng, model_seed = (
-    random_generator(ARGS.model_seed)
-    if ARGS.model_seed
-    else (default_rng, default_seed)
+seeds.update(
+    ARGS.kwargs(
+        ["A_train_seed", "model_seed", "D_train_seed", "learn_seed", "A_test_seed"],
+        ["A_train", "model", "D_train", "learn", "A_test"],
+    )
 )
-D_train_rng, D_train_seed = (
-    random_generator(ARGS.D_train_seed)
-    if ARGS.D_train_seed
-    else (default_rng, default_seed)
-)
-learn_rng, learn_seed = (
-    random_generator(ARGS.learn_seed)
-    if ARGS.learn_seed
-    else (default_rng, default_seed)
-)
-A_test_rng, A_test_seed = (
-    random_generator(ARGS.A_test_seed)
-    if ARGS.A_test_seed
-    else (default_rng, default_seed)
-)
-
 
 # Generate training data set of alternatives
-A_train = random_alternatives(ARGS.n_tr, ARGS.m, A_train_rng)
+A_train = random_alternatives(ARGS.n_tr, ARGS.m, default_rng(seeds["A_train"]))
 
 
 # Generate original model
 match ARGS.model:
     case "RMP":
-        Mo = random_rmp(ARGS.k_o, ARGS.m, model_rng)
+        Mo = random_rmp(ARGS.k_o, ARGS.m, default_rng(seeds["model"]))
     case "SRMP":
-        Mo = random_srmp(ARGS.k_o, ARGS.m, model_rng)
+        Mo = random_srmp(ARGS.k_o, ARGS.m, default_rng(seeds["model"]))
+# print(Mo)
 
 
 # Generate training binary comparisons
-D_train = random_comparisons(ARGS.n_bc, A_train, Mo, D_train_rng)
+D_train = random_comparisons(ARGS.n_bc, A_train, Mo, default_rng(seeds["D_train"]))
 
 
 # Create learner
 learner: Learner[RMP | SRMP]
 learn_kwargs: dict[str, Any] = {}
 match ARGS.method:
-
     case "MIP":
         # Create MIP leaner
         learner = MIP(
@@ -101,7 +88,7 @@ match ARGS.method:
             case "RMP":
                 neighbors.append(NeighborCapacities())
             case "SRMP":
-                neighbors.append(NeighborWeights(0.01))
+                neighbors.append(NeighborWeights(0.1))
         if ARGS.k_e >= 2:
             neighbors.append(NeighborLexOrder())
 
@@ -122,32 +109,58 @@ match ARGS.method:
         )
 
         # Add learning kwargs
+        seeds.update(
+            zip(
+                ["initial_model", "sa"],
+                SeedSequence(seeds["learn"]).generate_state(2),
+            )
+        )
         match ARGS.model:
             case "RMP":
                 learn_kwargs["initial_model"] = random_rmp(
-                    ARGS.k_e, ARGS.m, learn_rng, midpoints(A_train)
+                    ARGS.k_e,
+                    ARGS.m,
+                    default_rng(seeds["initial_model"]),
+                    midpoints(A_train),
                 )
             case "SRMP":
-                learn_kwargs["initial_model"] = random_srmp(ARGS.k_e, ARGS.m, learn_rng)
-        learn_kwargs["rng"] = learn_rng
+                learn_kwargs["initial_model"] = random_srmp(
+                    ARGS.k_e,
+                    ARGS.m,
+                    default_rng(seeds["initial_model"]),
+                    midpoints(A_train),
+                )
+        learn_kwargs["rng"] = default_rng(seeds["sa"])
 
 
 # Learn
+# print("learning")
 Me = learner.learn(A_train, D_train, **learn_kwargs)
+# print("learned")
 
 
 # Compare results
 if not Me:
     ValueError("No elicited model")
 else:
-    A_test = random_alternatives(ARGS.n_te, ARGS.m, A_test_rng)
+    A_test = random_alternatives(ARGS.n_te, ARGS.m, default_rng(seeds["A_test"]))
 
-    restauration = Me.fitness(A_train, D_train)
+    train_accuracy = Me.fitness(A_train, D_train)
+    test_accuracy = Me.fitness(A_test, all_comparisons(A_test, Mo))
 
     ranking_o = Mo.rank(A_test)
     ranking_e = Me.rank(A_test)
 
     kendall_tau = kendalltau(ranking_o.data, ranking_e.data)
 
-    print(f"Restauration rate : {restauration}")
-    print(f"Kendall's tau : {kendall_tau.statistic}")
+    # Print results
+    result: str = ""
+    result += str(train_accuracy) + " "
+    result += str(test_accuracy) + " "
+    result += str(kendall_tau.statistic) + " "
+    for seed in seeds.values():
+        result += str(seed) + " "
+    for arg in fields(ARGS):
+        result += str(getattr(ARGS, arg.name)) + " "
+
+    print(result)
