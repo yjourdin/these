179d16edcf67bc678853b46b5d4e243d501e93b2
Yann Jourdin <yann.jourdin@imt-atlantique.fr>
2023-12-12 17:34:32 +0100

    Simplify plot_results.py

--- results/plot_results.py
+++ results/plot_results.py
@@ -2,10 +2,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from pandas import read_csv
 
-# MIP benchmark
-
-MIP_df = read_csv(
-    "results_MIP.csv",
+gen_df = read_csv(
+    "csv/results_gen.csv",
     dtype={
         "N_tr": "category",
         "N_te": "category",
@@ -19,76 +17,15 @@ MIP_df = read_csv(
 )
 
 sns.relplot(
-    data=MIP_df,
-    x="N_bc",
-    y="Time",
-    style="K_e",
-    hue="K_e",
-    col="M",
-    kind="line",
-    errorbar="sd",
-    err_style="bars",
-    err_kws={"capsize": 5},
-)
-
-plt.savefig("MIP_time.png")
-
-sns.relplot(
-    data=MIP_df,
-    x="N_bc",
-    y="Train accuracy",
-    style="K_e",
-    hue="K_e",
-    col="M",
-    kind="line",
-    errorbar="sd",
-    err_style="bars",
-    err_kws={"capsize": 5},
-)
-
-plt.savefig("MIP_acc.png")
-
-
-# Evolutionnary benchmark
-
-evo_df = read_csv(
-    "results_evo.csv",
-    dtype={
-        "N_tr": "category",
-        "N_te": "category",
-        "M": "category",
-        "K_o": "category",
-        "K_e": "category",
-        # "N_bc": "category",
-        "Method": "category",
-        "Model": "category",
-    },
-)
-
-sns.relplot(
-    data=evo_df,
-    x="N_bc",
-    y="Time",
-    row="K_e",
-    col="M",
-    kind="line",
-    errorbar="sd",
-    err_style="bars",
-    err_kws={"capsize": 5},
-)
-
-plt.savefig("evo_time.png")
-
-sns.relplot(
-    data=evo_df,
+    data=gen_df,
     x="N_bc",
     y="Train accuracy",
     row="K_e",
     col="M",
     kind="line",
-    errorbar="sd",
+    errorbar=("ci", 95),
     err_style="bars",
     err_kws={"capsize": 5},
 )
 
-plt.savefig("evo_acc.png")
+plt.savefig("plots/gen_acc.png")
