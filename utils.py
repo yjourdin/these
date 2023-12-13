59895a9d42ab62efce1c273beab9f8eb3d6edd5c
Yann Jourdin <yann.jourdin@imt-atlantique.fr>
2023-12-13 16:35:27 +0100

    Add comments to midpoints computation

--- utils.py
+++ utils.py
@@ -4,12 +4,15 @@ from mcda_local.core.performance_table import NormalPerformanceTable, Performanc
 
 
 def midpoints(performance_table: PerformanceTable) -> PerformanceTable:
+    # sort performance table
     df = performance_table.normalize().data.transform(sort)
+
+    # compute midpoints
     df = df.rolling(2).mean().drop(df.index[0])
 
+    # Add 0's and 1's at the beginning and the end
     df.loc[0, :] = [0] * len(performance_table.criteria)
     df.loc[len(performance_table.data), :] = [1] * len(performance_table.criteria)
-
     df.sort_index(inplace=True)
 
     result = NormalPerformanceTable(
