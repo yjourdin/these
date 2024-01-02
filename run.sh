ef696898c646c720fd67a4c2c57737f123e02855
Yann Jourdin <yann.jourdin@imt-atlantique.fr>
2024-01-02 17:07:21 +0100

    Add temperatures in run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Output file
-file='results/csv/gen_out.csv'
+file='results/02-01-24.csv'
 
 # Header
 tr '\n' ',' <config/header.txt >$file
@@ -10,12 +10,14 @@ tr '\n' ',' <config/header.txt >$file
 M='M 7 11 15'
 K_e='K_e 1 2 3 4'
 N_bc='N_bc 100 300 500 1000 2000'
+T0='T0 0.1 0.03 0.02 0.01 0.005'
+Tf='Tf 0.001 0.0003 0.0002 0.0001 0.00005'
 seed='seed 0 1 2 3 4 5 6 7 8 9'
 
 # shellcheck disable=SC1083
 # shellcheck disable=SC2086
 parallel --header : \
     python main.py @config/defaults.txt \
-    --M={M} --K-o={K_e} --K-e={K_e} --N-bc={N_bc} --seed={seed} \
-    ::: $M ::: $K_e ::: $N_bc ::: $seed \
+    --M={M} --K-o={K_e} --K-e={K_e} --N-bc={N_bc} --T0={T0} --Tf={Tf} --seed={seed} \
+    ::: $M ::: $K_e ::: $N_bc :::+ $T0 :::+ $Tf ::: $seed \
     >>$file
