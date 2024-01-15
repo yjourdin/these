2ba71d7eae72b8a1d3f3e4ae8480c2a2364f38c9
Yann Jourdin <yann.jourdin@imt-atlantique.fr>
2024-01-15 18:38:40 +0100

    Fix bug in run scripts

--- run/RMP.sh
+++ run/RMP.sh
@@ -19,13 +19,12 @@ N_bc='N_bc 100 200 300 400 500 600 700 800 900 1000'
 T0='T0 0.01 0.005 0.003 0.0025 0.002 0.0016 0.0014 0.00125 0.0011 0.001'
 Tf='Tf 0.001 0.0005 0.0003 0.00025 0.0002 0.00016 0.00014 0.000125 0.00011 0.0001'
 error='error 0 0.1 0.2 0.3'
-repetition=10
 
 # shellcheck disable=SC1083
 # shellcheck disable=SC2086
-parallel -j35 --header : \
+parallel --header : \
     python main.py \
-    --N-tr=$N_tr --N-te=$N_te SA --model-o=RMP --model-e=RMP --alpha=$alpha --L=$L \
-    --M={M} --K-o={K_e} --K-e={K_e} --N-bc={N_bc} --T0={T0} --Tf={Tf} --error={error} \
-    ::: $M ::: $K_e ::: $N_bc :::+ $T0 :::+ $Tf ::: $error ::: $(seq 1 $repetition) \
+    --N-tr $N_tr --N-te $N_te SA --model-o RMP --model-e RMP --alpha $alpha --L $L \
+    --M {M} --K-o {K_e} --K-e {K_e} --N-bc {N_bc} --T0 {T0} --Tf {Tf} --error {error} \
+    ::: $M ::: $K_e ::: $N_bc :::+ $T0 :::+ $Tf ::: $error ::: {1..10} \
     >>$file
