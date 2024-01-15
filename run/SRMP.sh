9b0ef2b0a1bde5f1e0b007b02edc76330506df00
Yann Jourdin <yann.jourdin@imt-atlantique.fr>
2024-01-15 20:21:16 +0100

    Add repetition to run scripts

--- run/SRMP.sh
+++ run/SRMP.sh
@@ -19,6 +19,7 @@ N_bc='N_bc 100 300 500 1000 2000'
 T0='T0 0.01 0.003 0.002 0.001 0.0005'
 Tf='Tf 0.001 0.0003 0.0002 0.0001 0.00005'
 error='error 0 0.1 0.2 0.3'
+repetition=50
 
 # shellcheck disable=SC1083
 # shellcheck disable=SC2086
@@ -26,5 +27,5 @@ parallel -j75 --header : \
     python main.py \
     --N-tr $N_tr --N-te $N_te SA --model-o SRMP --model-e SRMP --alpha $alpha --L $L \
     --M {M} --K-o {K_e} --K-e {K_e} --N-bc {N_bc} --T0 {T0} --Tf {Tf} --error {error} \
-    ::: $M ::: $K_e ::: $N_bc :::+ $T0 :::+ $Tf ::: $error ::: {1..50} \
+    ::: $M ::: $K_e ::: $N_bc :::+ $T0 :::+ $Tf ::: $error ::: repetition $(seq $repetition) \
     >>$file
