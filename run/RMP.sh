909ea0996649baaae94ce5cd48583042454b9c96
Yann Jourdin <yann.jourdin@imt-atlantique.fr>
2024-01-23 13:42:03 +0100

    Modify run scripts to correspond to MIC experiments

--- run/RMP.sh
+++ run/RMP.sh
@@ -20,7 +20,7 @@ K_e='K_e 1 2 3'
 N_bc='N_bc 100 200 300 400 500 600 700 800 900 1000'
 T0='T0 0.01 0.005 0.003 0.0025 0.002 0.0016 0.0014 0.00125 0.0011 0.001'
 Tf='Tf 0.001 0.0005 0.0003 0.00025 0.0002 0.00016 0.00014 0.000125 0.00011 0.0001'
-error='error 0 0.1 0.2 0.3'
+error='error 0'
 repetition=10
 
 # shellcheck disable=SC1083
@@ -30,5 +30,5 @@ parallel -j35 --header : \
     --N-tr $N_tr --N-te $N_te SA --model-o RMP --model-e RMP --alpha $alpha --L $L \
     --M {M} --K-o {K_o} --K-e {K_e} --N-bc {N_bc} --T0 {T0} --Tf {Tf} --error {error} \
     ::: repetition $(seq $repetition) \
-    ::: $M ::: $K_o ::: $K_e ::: $N_bc :::+ $T0 :::+ $Tf ::: $error \
+    ::: $M ::: $K_o :::+ $K_e ::: $N_bc :::+ $T0 :::+ $Tf ::: $error \
     >>"$file"
--- run/SRMP.sh
+++ run/SRMP.sh
@@ -20,7 +20,7 @@ K_e='K_e 1 2 3 4'
 N_bc='N_bc 100 300 500 1000 2000'
 T0='T0 0.01 0.003 0.002 0.001 0.0005'
 Tf='Tf 0.001 0.0003 0.0002 0.0001 0.00005'
-error='error 0 0.1 0.2 0.3'
+error='error 0 '
 repetition=50
 
 # shellcheck disable=SC1083
@@ -30,5 +30,5 @@ parallel -j35 --header : \
     --N-tr $N_tr --N-te $N_te SA --model-o SRMP --model-e SRMP --alpha $alpha --L $L \
     --M {M} --K-o {K_o} --K-e {K_e} --N-bc {N_bc} --T0 {T0} --Tf {Tf} --error {error} \
     ::: repetition $(seq $repetition) \
-    ::: $M ::: $K_o ::: $K_e ::: $N_bc :::+ $T0 :::+ $Tf ::: $error \
+    ::: $M ::: $K_o :::+ $K_e ::: $N_bc :::+ $T0 :::+ $Tf ::: $error \
     >>"$file"
