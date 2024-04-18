5adfbb4a81765df6d30c329ae451d3fb19dcba3d
Yann Jourdin <yann.jourdin@imt-atlantique.fr>
2024-04-18 18:55:02 +0200

    task precedence in its own file

--- run/__main__.py
+++ run/__main__.py
@@ -1,25 +1,15 @@
 import logging.config
-from collections import defaultdict
 from multiprocessing import JoinableQueue, Process, Queue
 from threading import Thread
+from typing import cast
 
 from numpy.random import default_rng
 
 from .argument_parser import parse_args
 from .logging import create_logging_config_dict, logger_thread
 from .path import Directory
-from .task import (
-    Task,
-    TaskExecutor,
-    task_A_test,
-    task_A_train,
-    task_D,
-    task_manager,
-    task_MIP,
-    task_Mo,
-    task_SA,
-    task_test,
-)
+from .precedence import task_precedence
+from .task import TaskExecutor, task_manager
 from .worker import file_thread, worker
 
 # Parse arguments
@@ -33,7 +23,9 @@ dir.mkdir()
 seeds = (
     args.seeds
     if isinstance(args.seeds, list)
-    else default_rng(args.seed).integers(2**63, size=args.seeds)
+    else cast(
+        list[int], default_rng(args.seed).integers(2**63, size=args.seeds).tolist()
+    )
 )
 with dir.seeds_file.open("w") as f:
     for i, seed in enumerate(seeds):
@@ -47,88 +39,11 @@ train_results_queue: Queue = Queue()
 test_results_queue: Queue = Queue()
 
 # Set up task precedence
-succeed: defaultdict[Task, list[Task]] = defaultdict(list)
-precede: defaultdict[Task, list[Task]] = defaultdict(list)
+to_do, succeed, precede = task_precedence(args)
 
-for i in range(len(seeds)):
-    for m in args.M:
-        for n_te in args.N_te:
-            t_A_test = task_A_test(i, n_te, m)
-            task_queue.put(t_A_test)  # Put task in task queue
-    for n_tr in args.N_tr:
-        t_A_train = task_A_train(i, n_tr, m)
-        task_queue.put(t_A_train)  # Put task in task queue
-        for Mo in args.Mo:
-            for ko in args.Ko:
-                t_Mo = task_Mo(i, m, Mo, ko)
-                task_queue.put(t_Mo)  # Put task in task queue
-                for n_bc in args.N_bc:
-                    for e in args.error:
-                        t_D = task_D(i, n_tr, m, Mo, ko, n_bc, e)
-                        succeed[t_A_train] += [t_D]
-                        succeed[t_Mo] += [t_D]
-                        precede[t_D] += [t_A_train, t_Mo]
-                        for Me in args.Me:
-                            for ke in args.Ke:
-                                ts = []
-                                for method in args.method:
-                                    match method:
-                                        case "SA":
-                                            for config in range(len(args.config) or 1):
-                                                t_Me = task_SA(
-                                                    i,
-                                                    n_tr,
-                                                    m,
-                                                    Mo,
-                                                    ko,
-                                                    n_bc,
-                                                    e,
-                                                    Me,
-                                                    ke,
-                                                    config,
-                                                )
-                                                t_test = task_test(
-                                                    i,
-                                                    n_tr,
-                                                    n_te,
-                                                    m,
-                                                    Mo,
-                                                    ko,
-                                                    n_bc,
-                                                    e,
-                                                    Me,
-                                                    ke,
-                                                    method,
-                                                    config,
-                                                )
-                                                ts.append((t_Me, t_test))
-                                        case "MIP" if Me == "SRMP":
-                                            t_Me = task_MIP(
-                                                i, n_tr, m, Mo, ko, n_bc, e, ke
-                                            )
-                                            t_test = task_test(
-                                                i,
-                                                n_tr,
-                                                n_te,
-                                                m,
-                                                Mo,
-                                                ko,
-                                                n_bc,
-                                                e,
-                                                Me,
-                                                ke,
-                                                method,
-                                            )
-                                            ts.append((t_Me, t_test))
-                                        case _:
-                                            break
-                                    for n_te in args.N_te:
-                                        for t_Me, t_test in ts:
-                                            succeed[t_D] += [t_Me]
-                                            precede[t_Me] += [t_D]
-                                            succeed[t_Me] += [t_test]
-                                            succeed[t_A_test] += [t_test]
-                                            precede[t_test] += [t_A_test, t_Me]
+# Populate task_queue
+for task in to_do:
+    task_queue.put(task)
 
 # Create task executor
 task_executor = TaskExecutor(
@@ -137,7 +52,6 @@ task_executor = TaskExecutor(
     seeds,
     train_results_queue,
     test_results_queue,
-    args.config if hasattr(args, "config") else {},
 )
 
 # Start result file threads
@@ -187,9 +101,9 @@ done_queue.put("STOP")
 # Stop result file threads
 train_results_queue.put("STOP")
 test_results_queue.put("STOP")
-train_result_thread.join()
-test_result_thread.join()
+# train_result_thread.join()
+# test_result_thread.join()
 
 # Stop logging thread
 logging_queue.put("STOP")
-logging_thread.join()
+# logging_thread.join()
---
+++ run/precedence.py
@@ -0,0 +1,103 @@
+from collections import defaultdict
+
+from .arguments import Arguments
+from .task import (
+    Task,
+    task_A_test,
+    task_A_train,
+    task_D,
+    task_MIP,
+    task_Mo,
+    task_SA,
+    task_test,
+)
+
+
+def task_precedence(args: Arguments):
+    succeed: defaultdict[Task, list[Task]] = defaultdict(list)
+    precede: defaultdict[Task, list[Task]] = defaultdict(list)
+    to_do: list[Task] = []
+
+    nb_DM = args.seeds if isinstance(args.seeds, int) else len(args.seeds)
+
+    for i in range(nb_DM):
+        for m in args.M:
+            for n_te in args.N_te:
+                t_A_test = task_A_test(i, n_te, m)
+                to_do.append(t_A_test)  # Put task in task queue
+        for n_tr in args.N_tr:
+            t_A_train = task_A_train(i, n_tr, m)
+            to_do.append(t_A_train)  # Put task in task queue
+            for Mo in args.Mo:
+                for ko in args.Ko:
+                    t_Mo = task_Mo(i, m, Mo, ko)
+                    to_do.append(t_Mo)  # Put task in task queue
+                    for n_bc in args.N_bc:
+                        for e in args.error:
+                            t_D = task_D(i, n_tr, m, Mo, ko, n_bc, e)
+                            succeed[t_A_train] += [t_D]
+                            succeed[t_Mo] += [t_D]
+                            precede[t_D] += [t_A_train, t_Mo]
+                            for Me in args.Me:
+                                for ke in args.Ke:
+                                    ts = []
+                                    for method in args.method:
+                                        match method:
+                                            case "SA":
+                                                for config in args.config:
+                                                    t_Me = task_SA(
+                                                        i,
+                                                        n_tr,
+                                                        m,
+                                                        Mo,
+                                                        ko,
+                                                        n_bc,
+                                                        e,
+                                                        Me,
+                                                        ke,
+                                                        config,
+                                                    )
+                                                    t_test = task_test(
+                                                        i,
+                                                        n_tr,
+                                                        n_te,
+                                                        m,
+                                                        Mo,
+                                                        ko,
+                                                        n_bc,
+                                                        e,
+                                                        Me,
+                                                        ke,
+                                                        method,
+                                                        config,
+                                                    )
+                                                    ts.append((t_Me, t_test))
+                                            case "MIP" if Me == "SRMP":
+                                                t_Me = task_MIP(
+                                                    i, n_tr, m, Mo, ko, n_bc, e, ke
+                                                )
+                                                t_test = task_test(
+                                                    i,
+                                                    n_tr,
+                                                    n_te,
+                                                    m,
+                                                    Mo,
+                                                    ko,
+                                                    n_bc,
+                                                    e,
+                                                    Me,
+                                                    ke,
+                                                    method,
+                                                )
+                                                ts.append((t_Me, t_test))
+                                            case _:
+                                                break
+                                        for n_te in args.N_te:
+                                            for t_Me, t_test in ts:
+                                                succeed[t_D] += [t_Me]
+                                                precede[t_Me] += [t_D]
+                                                succeed[t_Me] += [t_test]
+                                                succeed[t_A_test] += [t_test]
+                                                precede[t_test] += [t_A_test, t_Me]
+
+    return to_do, succeed, precede
