cd1e2ff4e046802a9327cd8c52bfb170f7cf3e2b
Yann Jourdin <yann.jourdin@imt-atlantique.fr>
2024-06-10 16:48:04 +0200

    Update configs in args.json

--- grid_search.py
+++ grid_search.py
@@ -2,22 +2,22 @@ from json import dump, load
 
 from run.config import SAConfig
 
-configs = {}
+configs = []
 
 MAX_ITER = 20_000
-
-id = 0
-for T0_coef in [0.1, 0.5, 1, 5, 10]:
-    for alpha in [0.999, 0.9995, 0.9999, 0.99995, 0.99999]:
-        for amp in [0.1, 0.2, 0.3, 0.4, 0.5]:
-            configs[id] = SAConfig(T0_coef, alpha, amp, MAX_ITER).to_dict()
-            id += 1
 
 with open("args.json", "r+") as f:
     args = load(f)
     f.seek(0)
 
-    args["config"]["SA"] = configs
+    id = 0
+    for T0_coef in [0.1, 0.5, 1, 5, 10]:
+        for alpha in [0.999, 0.9995, 0.9999, 0.99995, 0.99999]:
+            for amp in [0.1, 0.2, 0.3, 0.4, 0.5] if "SRMP" in args["Me"] else [0]:
+                configs.append(SAConfig(id, T0_coef, alpha, amp, MAX_ITER))
+                id += 1
+
+    args["config"] = configs
 
     dump(args, f, indent=4)
     f.truncate()
