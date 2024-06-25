b94e7b6e309c338430a4258c14ccb4a164057c54
Yann Jourdin <yann.jourdin@imt-atlantique.fr>
2024-06-25 16:19:52 +0200

    add Rscript to create report files

---
+++ tuning/report.R
@@ -0,0 +1,7 @@
+library("iraceplot")
+
+args <- commandArgs(trailingOnly = TRUE)
+
+iraceResults <- irace::read_logfile(file.path(args[1], "irace.Rdata"))
+
+report(iraceResults, filename = file.path(args[1], "report"))
