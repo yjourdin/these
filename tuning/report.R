add66f6da62bdbb327959136c5b8d5c6740c6e17
Yann Jourdin <yann.jourdin@imt-atlantique.fr>
2024-06-26 17:53:42 +0200

    Update tuning

--- tuning/report.R
+++ tuning/report.R
@@ -4,4 +4,8 @@ args <- commandArgs(trailingOnly = TRUE)
 
 iraceResults <- irace::read_logfile(file.path(args[1], "irace.Rdata"))
 
-report(iraceResults, filename = file.path(args[1], "report"))
+report(
+    iraceResults,
+    filename = file.path(args[1], "report"),
+    list(experiments_matrix = NULL, convergence = TRUE)
+)
