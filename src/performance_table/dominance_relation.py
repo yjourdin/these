df5975eccbe61c723238a0863d96c988e806fc19
Yann Jourdin <yann.jourdin@imt-atlantique.fr>
2024-09-17 11:27:32 +0200

    dominance_relation tiny improvement

--- src/performance_table/dominance_relation.py
+++ src/performance_table/dominance_relation.py
@@ -2,17 +2,18 @@ from itertools import combinations
 from typing import Any
 
 from mcda import PerformanceTable
+from mcda.relations import PreferenceStructure, P
 
 DominanceRelation = set[tuple[Any, Any]]
 
 
 def dominance_relation(performance_table: PerformanceTable):
-    result: DominanceRelation = set()
+    result = PreferenceStructure()
     alternatives_values = performance_table.alternatives_values
     values = {a: alternatives_values[a] for a in performance_table.alternatives}
     for a, b in combinations(performance_table.alternatives, 2):
         if values[a].dominate(values[b]):
-            result.add((a, b))
+            result._relations.append(P(a, b))
         if values[b].dominate(values[a]):
-            result.add((b, a))
+            result._relations.append(P(b, a))
     return result
