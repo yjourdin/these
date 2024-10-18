c1716aa91f10b2df5b75890efa7f09f474224135
Yann Jourdin <yann.jourdin@imt-atlantique.fr>
2024-10-18 14:03:27 +0200

    Organise neighbors

--- src/sa/neighbor.py
+++ src/sa/neighbor.py
@@ -1,14 +1,17 @@
 from abc import ABC, abstractmethod
 from collections.abc import Sequence
 from copy import deepcopy
+from dataclasses import InitVar, dataclass
 from itertools import chain, combinations
 from typing import Any, Collection, cast
 
 import numpy as np
 from mcda import PerformanceTable
+from more_itertools import powerset
 from numpy.random import Generator
 
-from ..rmp.model import RMPModelCapacity
+from ..dataclass import Dataclass
+from ..rmp.model import RMPModelCapacity, RMPModelCapacityInt
 from ..srmp.model import SRMPModel
 
 
@@ -35,133 +38,115 @@ class RandomNeighbor[S](Neighbor[S]):
         return self.neighbors[i](sol, rng)
 
 
-class NeighborProfiles(Neighbor[SRMPModel | RMPModelCapacity]):
-    def __init__(self, values: PerformanceTable):
-        self.values = values
+@dataclass
+class NeighborProfile(Neighbor[SRMPModel | RMPModelCapacity], Dataclass):
+    amp: float = 1
 
     def __call__(self, sol, rng):
         neighbor = deepcopy(sol)
 
-        # crit_ind = rng.choice(len(neighbor.profiles.criteria))
-        # profile_ind = rng.choice(len(neighbor.profiles.alternatives))
-        # value = neighbor.profiles.data.iloc[profile_ind, crit_ind]
+        crit_ind = rng.choice(len(neighbor.profiles.criteria))
+        profile_ind = rng.choice(len(neighbor.profiles.alternatives))
+        profile_perf = cast(float, neighbor.profiles.data.iloc[profile_ind, crit_ind])
 
-        # neighbor.profiles.data.iloc[profile_ind, crit_ind] = rng.uniform(
-        #     max(value - self.amp, 0), min(value + self.amp, 1)
-        # )
+        profile_perf = rng.uniform(
+            max(profile_perf - self.amp, 0), min(profile_perf + self.amp, 1)
+        )
+
+        neighbor.profiles.data.iloc[profile_ind, crit_ind] = profile_perf
+
+        neighbor.profiles.data.iloc[:, crit_ind] = neighbor.profiles.data.iloc[
+            :, crit_ind
+        ].sort_values()
+
+        return neighbor
+
+
+@dataclass
+class NeighborProfileDiscretized(Neighbor[SRMPModel | RMPModelCapacity], Dataclass):
+    values: PerformanceTable
+    local: bool = False
+
+    def __call__(self, sol, rng):
+        neighbor = deepcopy(sol)
 
         crit_ind = rng.choice(len(neighbor.profiles.criteria))
         crit_values = self.values.data.iloc[:, crit_ind]
         profile_ind = rng.choice(len(neighbor.profiles.alternatives))
+        profile_perf = cast(float, neighbor.profiles.data.iloc[profile_ind, crit_ind])
+        profile_perf_ind = cast(int, crit_values[crit_values == profile_perf].index[0])
 
-        # value = neighbor.profiles.data.iloc[profile_ind, crit_ind]
-        # value_ind = cast(
-        #     int, crit_values.index.get_loc(crit_values[crit_values == value].index[0])
-        # )
-        # if value_ind == 0:
-        #     neighbor.profiles.data.iloc[profile_ind, crit_ind] = crit_values[
-        #         crit_values.index[value_ind + 1]
-        #     ]
-        # elif value_ind == (len(self.values.alternatives) - 1):
-        #     neighbor.profiles.data.iloc[profile_ind, crit_ind] = crit_values[
-        #         crit_values.index[value_ind - 1]
-        #     ]
-        # else:
-        #     new_value_ind = rng.choice([value_ind - 1, value_ind + 1])
-        #     neighbor.profiles.data.iloc[profile_ind, crit_ind] = crit_values[
-        #         crit_values.index[new_value_ind]
-        #     ]
+        if self.local:
+            available_ind = []
+            if profile_perf_ind > 0:
+                available_ind.append(profile_perf_ind - 1)
+            if profile_perf_ind < (len(self.values.alternatives) - 1):
+                available_ind.append(profile_perf_ind + 1)
+        else:
+            available_ind = range(len(self.values.alternatives))
+        profile_perf_ind = rng.choice(available_ind)
 
-        new_value_ind = rng.choice(len(self.values.alternatives))
         neighbor.profiles.data.iloc[profile_ind, crit_ind] = crit_values[
-            crit_values.index[new_value_ind]
+            crit_values.index[profile_perf_ind]
         ]
 
         neighbor.profiles.data.iloc[:, crit_ind] = neighbor.profiles.data.iloc[
             :, crit_ind
         ].sort_values()
-        return neighbor
-
-
-class NeighborProfilesSRMP(Neighbor[SRMPModel]):
-    def __init__(self, values: PerformanceTable):
-        self.values = values
-
-    def __call__(self, sol, rng):
-        neighbor = deepcopy(sol)
-
-        crit_ind = rng.choice(
-            [
-                i
-                for i, x in enumerate(neighbor.profiles.criteria)
-                if neighbor.weights[x] != 0
-            ]
-        )
-        crit_values = self.values.data.iloc[:, crit_ind]
-        profile_ind = rng.choice(len(neighbor.profiles.alternatives))
-        value = neighbor.profiles.data.iloc[profile_ind, crit_ind]
-        value_ind = cast(int, crit_values[crit_values == value].index[0])
-
-        if value_ind == 0:
-            neighbor.profiles.data.iloc[profile_ind, crit_ind] = crit_values[
-                value_ind + 1
-            ]
-        elif value_ind == (len(self.values.alternatives) - 1):
-            neighbor.profiles.data.iloc[profile_ind, crit_ind] = crit_values[
-                value_ind - 1
-            ]
-        else:
-            new_value_ind = rng.choice([value_ind - 1, value_ind + 1])
-            neighbor.profiles.data.iloc[profile_ind, crit_ind] = crit_values[
-                new_value_ind
-            ]
-
-        neighbor.profiles.data.transform(np.sort)
 
         return neighbor
 
 
-class NeighborWeights(Neighbor[SRMPModel]):
-    def __init__(self, amp: float):
-        self.amp = amp
+@dataclass
+class NeighborWeight(Neighbor[SRMPModel], Dataclass):
+    amp: float = 1
 
     def __call__(self, sol, rng):
         neighbor = deepcopy(sol)
 
         crit_ind = rng.choice(len(neighbor.weights))
-
-        neighbor.weights[crit_ind] = rng.uniform(
-            max(neighbor.weights[crit_ind] - self.amp, 0),
-            min(neighbor.weights[crit_ind] + self.amp, 1),
-        )
-
-        # neighbor.weights[crit_ind] = max(
-        #     min(rng.normal(neighbor.weights[crit_ind], self.amp), 1), 0
-        # )
+        weight = neighbor.weights[crit_ind]
+        weight = rng.uniform(max(weight - self.amp, 0), min(weight + self.amp, 1))
+        neighbor.weights[crit_ind] = weight
 
         s = sum(neighbor.weights)
         neighbor.weights = [w / s for w in neighbor.weights]
 
-        # weight = int(d[crit] + 0.99)
+        return neighbor
 
-        # max = max_weight(len(d))
 
-        # if 1 < weight < max:
-        #     new_weight = rng.choice([weight - 1, weight + 1])
-        # elif 1 == weight:
-        #     new_weight = weight + 1
-        # elif weight == max:
-        #     new_weight = weight - 1
-        # else:
-        #     raise ValueError(f"{weight} is not between 0 and {max}")
+@dataclass
+class NeighborWeightDiscretized(Neighbor[SRMPModel]):
+    max: int
+    local: bool = False
 
-        # d[crit] = new_weight
+    def __call__(self, sol, rng):
+        neighbor = deepcopy(sol)
+
+        crit_ind = rng.choice(len(neighbor.weights))
+        weight = neighbor.weights[crit_ind]
+
+        if self.local:
+            available_ind = []
+            if weight > 0:
+                available_ind.append(weight - 1)
+            if weight < self.max:
+                available_ind.append(weight + 1)
+        else:
+            available_ind = range(self.max + 1)
+        weight = rng.choice(available_ind)
+
+        neighbor.weights[crit_ind] = weight
 
         return neighbor
 
 
-class NeighborCapacity(Neighbor[RMPModelCapacity]):
-    def __init__(self, s: Collection[Any]):
+@dataclass
+class NeighborCapacity(Neighbor[RMPModelCapacity], Dataclass):
+    s: InitVar[Collection[Any]]
+    amp: float = 1
+
+    def __post_init__(self, s: Collection[Any]):
         s = set(s)
 
         power_set_tmp = chain.from_iterable(
@@ -177,42 +162,78 @@ class NeighborCapacity(Neighbor[RMPModelCapacity]):
 
         capacities = neighbor.capacity
         keys = list(capacities)
-        crits = keys[rng.choice(len(keys))]
+        coalition = keys[rng.choice(len(keys))]
+        capacity = capacities[coalition]
 
-        infimum_capacities = [capacities[ss] for ss in self.infimum[crits]]
+        infimum_capacities = [capacities[ss] for ss in self.infimum[coalition]]
         min_capacity = max(infimum_capacities) if infimum_capacities else 0
-        supremum_capacities = [capacities[ss] for ss in self.supremum[crits]]
+        supremum_capacities = [capacities[ss] for ss in self.supremum[coalition]]
         max_capacity = min(supremum_capacities) if supremum_capacities else 1
 
-        new_capa = rng.uniform(min_capacity, max_capacity)
-        # if power_set.min_capacity(crits) < capacity < power_set.max_capacity(crits):
-        #     new_capa = rng.choice([capacity - 1, capacity + 1])
-        # elif power_set.min_capacity(crits) == capacity:
-        #     new_capa = capacity + 1
-        # elif capacity == power_set.max_capacity(crits):
-        #     new_capa = capacity - 1
-        # else:
-        #     raise ValueError(
-        #         f"{capacity} is not between {power_set.min_capacity(crits)}"
-        #         f" and {power_set.max_capacity(crits)}"
-        #     )
-        neighbor.capacity[crits] = new_capa
+        capacity = rng.uniform(min_capacity, max_capacity)
+        neighbor.capacity[coalition] = capacity
+
         return neighbor
 
 
-class NeighborLexOrder(Neighbor[SRMPModel | RMPModelCapacity]):
+@dataclass
+class NeighborCapacityDiscretized(Neighbor[RMPModelCapacityInt]):
+    s: InitVar[Collection[Any]]
+    local: bool = False
+
+    def __post_init__(self, s: Collection[Any]):
+        s = set(s)
+
+        power_set = {frozenset(x) for x in powerset(s)}
+
+        self.supremum = {ss: {ss | {i} for i in (s - ss)} for ss in power_set}
+        self.infimum = {ss: {ss - {i} for i in ss} for ss in power_set}
+
     def __call__(self, sol, rng):
         neighbor = deepcopy(sol)
+
+        capacities = neighbor.capacity
+        keys = list(capacities)
+        coalition = keys[rng.choice(len(keys))]
+        capacity = capacities[coalition]
+
+        infimum_capacities = [capacities[ss] for ss in self.infimum[coalition]]
+        min_capacity = max(infimum_capacities) if infimum_capacities else 0
+        supremum_capacities = [capacities[ss] for ss in self.supremum[coalition]]
+        max_capacity = min(supremum_capacities) if supremum_capacities else 1
+
+        if self.local:
+            available_capacity = []
+            if capacity > min_capacity:
+                available_capacity.append(capacity - 1)
+            if capacity > max_capacity:
+                available_capacity.append(capacity + 1)
+        else:
+            available_capacity = range(min_capacity, max_capacity + 1)
+        capacity = rng.choice(available_capacity)
+        neighbor.capacity[coalition] = capacity
+
+        return neighbor
+
+
+@dataclass
+class NeighborLexOrder(Neighbor[SRMPModel | RMPModelCapacity], Dataclass):
+    local: bool = False
+
+    def __call__(self, sol, rng):
+        neighbor = deepcopy(sol)
+
         lex_order = neighbor.lexicographic_order
-        i = rng.choice(len(lex_order))
-        j = rng.choice([x for x in range(len(lex_order)) if x != i])
+
+        if self.local:
+            i = rng.choice(len(lex_order) - 1)
+            j = i + 1
+        else:
+            i = rng.choice(len(lex_order))
+            j = rng.choice([x for x in range(len(lex_order)) if x != i])
         neighbor.lexicographic_order[i], neighbor.lexicographic_order[j] = (
             lex_order[j],
             lex_order[i],
         )
-        # i = rng.choice(len(lex_order) - 1)
-        # neighbor.lexicographic_order[i], neighbor.lexicographic_order[i + 1] = (
-        #     lex_order[i + 1],
-        #     lex_order[i],
-        # )
+
         return neighbor
