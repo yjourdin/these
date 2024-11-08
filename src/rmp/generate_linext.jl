7da0df30508dd37ed63800166d8df571e556a853
Yann Jourdin <yann.jourdin@imt-atlantique.fr>
2024-11-08 11:38:43 +0100

    clean julia files

--- src/rmp/generate_linext.jl
+++
@@ -1,8 +0,0 @@
-include("GenerateLinext.jl")
-
-using Random
-using SimplePosets
-using .GenerateLinext
-
-Random.seed!(parse(Int, ARGS[2]))
-println(generate_linext(BooleanLattice(parse(Int, ARGS[1])), Random.default_rng()))
