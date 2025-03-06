94d798907aa13ca5282f6fbe1ff3e530c4a248cb
Yann Jourdin <yann.jourdin@imt-atlantique.fr>
2025-03-06 10:38:13 +0100

    update julia python call

--- src/julia/function.py
+++ src/julia/function.py
@@ -8,21 +8,26 @@ from .file import PARENT_DIR, S_file, WE_dir
 
 
 def run_julia(scriptname: str, *args: Any, **kwargs: Any):
-    return run(
+    result = run(
         ["julia", PARENT_DIR / scriptname]
         + [str(x) for x in args]
-        + list(chain.from_iterable((f"--{k}", str(v)) for k, v in kwargs.items())),
+        + list(
+            chain.from_iterable(
+                (f"--{k}", str(v)) for k, v in kwargs.items() if v is not None
+            )
+        ),
         capture_output=True,
         text=True,
     ).stdout
 
+    try:
+        return ast.literal_eval(result)
+    except Exception:
+        raise Exception(f"Julia output : {result}")
+
 
 def generate_linext(m: int, seed: Seed | None = None) -> list[list[bool]]:
-    linext = ast.literal_eval(
-        run_julia(
-            "generate_linext.jl", m, **({"seed": seed} if seed is not None else {})
-        )
-    )
+    linext = run_julia("generate_linext.jl", m, seed=seed)
     return [[bool(int(x)) for x in node] for node in linext]
 
 
@@ -36,23 +41,9 @@ def generate_weak_order(m: int, seed: Seed | None = None) -> list[int]:
     if not file.exists():
         generate_partial_sum(m)
 
-    return ast.literal_eval(
-        run_julia(
-            "generate_weak_order.jl",
-            m,
-            file,
-            **({"seed": seed} if seed is not None else {}),
-        )
-    )
+    return run_julia("generate_weak_order.jl", m, file, seed=seed)
 
 
 def generate_weak_order_ext(m: int, seed: Seed | None = None) -> list[list[list[bool]]]:
-    weak_order = ast.literal_eval(
-        run_julia(
-            "generate_weak_order_ext.jl",
-            m,
-            WE_dir(m),
-            **({"seed": seed} if seed is not None else {}),
-        )
-    )
+    weak_order = run_julia("generate_weak_order_ext.jl", m, WE_dir(m), seed=seed)
     return [[[bool(int(x)) for x in node] for node in block] for block in weak_order]
