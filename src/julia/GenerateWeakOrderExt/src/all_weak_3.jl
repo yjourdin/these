35b04adfcb5467ef60cbc0182fc2dde2610b2025
Yann Jourdin <yann.jourdin@imt-atlantique.fr>
2025-10-17 17:40:50 +0200

    undo previous changes

--- src/julia/GenerateWeakOrderExt/src/all_weak_3.jl
+++ src/julia/GenerateWeakOrderExt/src/all_weak_3.jl
@@ -9,7 +9,7 @@ function AllWeak3!(labels, P, Y, A)
         get(labels, i, nothing) ≠ A′ || continue
 
         insert!(labels, i, A′)
-        # println("Vertices created : $(length(labels))")
+        # @debug "Vertices created : $(length(labels))"
         Y′ = @chain B begin
             filter(P)
             Bit.union(Y)
